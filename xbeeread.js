e7922ffb4f184c3735c02e718e146786d2281efc
Michael Teeuw <michael@xonaymedia.nl>
2014-04-21 15:42:42 +0200

    first commit

---
+++ xbeeread.js
@@ -0,0 +1,46 @@
+//setup socket io server
+var express = require('express');
+var app = express();
+var io = require('socket.io').listen(app.listen(8080), { log: false });
+
+//set up xbee system
+var xbee = require('xbee-api');
+var xbeeAPI = new xbee.XBeeAPI({
+  api_mode: 1
+});
+
+//open serial port
+var SerialPort = require('serialport').SerialPort;
+var serialport = new SerialPort("/dev/ttyUSB0", {
+  baudrate: 9600,
+  parser: xbeeAPI.rawParser()
+});
+
+//create helper vars
+var dishwasherDone = false;
+
+//wait for connection
+io.sockets.on('connection', function (socket) {
+
+	//send current state
+	socket.emit('dishwasher', dishwasherDone);
+
+	//read xbee frames
+	xbeeAPI.on("frame_object", function(frame) {
+
+		//handle input for Dishwasher. 
+		if (frame.remote16 == '0001') {
+			var done = (frame.data[4] === 0) ? true : false;
+
+			if (done != dishwasherDone) {
+				dishwasherDone = done;
+				var now = new Date;
+				console.log(now + ' - Dishwasher done: ' + dishwasherDone);
+
+				//broadcast new state to socket clients
+				socket.emit('dishwasher', dishwasherDone);
+			}
+		}
+
+	});
+});
