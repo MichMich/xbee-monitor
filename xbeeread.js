206bd0d011240484bddc11f1285b9cf77d05a1ee
Michael Teeuw <michael@xonaymedia.nl>
2014-04-21 16:28:25 +0200

    broadcast fix

--- xbeeread.js
+++ xbeeread.js
@@ -6,41 +6,46 @@ var io = require('socket.io').listen(app.listen(8080), { log: false });
 //set up xbee system
 var xbee = require('xbee-api');
 var xbeeAPI = new xbee.XBeeAPI({
-  api_mode: 1
+	api_mode: 1
 });
 
 //open serial port
 var SerialPort = require('serialport').SerialPort;
 var serialport = new SerialPort("/dev/ttyUSB0", {
-  baudrate: 9600,
-  parser: xbeeAPI.rawParser()
+	baudrate: 9600,
+	parser: xbeeAPI.rawParser()
 });
+
 
 //create helper vars
 var dishwasherDone = false;
 
+
 //wait for connection
 io.sockets.on('connection', function (socket) {
+
+	console.log('socket connected');
 
 	//send current state
 	socket.emit('dishwasher', dishwasherDone);
 
-	//read xbee frames
-	xbeeAPI.on("frame_object", function(frame) {
+});
 
-		//handle input for Dishwasher. 
-		if (frame.remote16 == '0001') {
-			var done = (frame.data[4] === 0) ? true : false;
+//handle xbee data
+xbeeAPI.on("frame_object", function(frame) {
 
-			if (done != dishwasherDone) {
-				dishwasherDone = done;
-				var now = new Date;
-				console.log(now + ' - Dishwasher done: ' + dishwasherDone);
+	//handle input for Dishwasher. 
+	if (frame.remote16 == '0001') {
+		var done = (frame.data[4] === 0) ? true : false;
 
-				//broadcast new state to socket clients
-				socket.emit('dishwasher', dishwasherDone);
-			}
+		if (done != dishwasherDone) {
+			dishwasherDone = done;
+			var now = new Date;
+			console.log(now + ' - Dishwasher done: ' + dishwasherDone);
+
+			//broadcast new state to socket clients
+			io.sockets.emit('dishwasher', dishwasherDone);
 		}
+	}
 
-	});
 });
